2f37c5edcffe963f4af29ab1d01869be52f2dcae
Harlan Wilton <harlan@harlanzw.com>
2022-09-07 18:31:00 +1000

    feat: add `resolveKey` option to packObject

--- src/pack/object.ts
+++ src/pack/object.ts
@@ -4,11 +4,15 @@ function asArray(input: any) {
   return Array.isArray(input) ? input : [input]
 }
 
+export const InternalKeySymbol = '_$key'
+
 export function packObject<T extends Record<string, any>>(input: T, options?: PackOptions<T>): Partial<T> {
   const keys = Object.keys(input) as (keyof T)[]
   let [k, v] = keys
-  if (!options)
-    options = { key: k, value: v }
+  options = options || {}
+  options.key = options.key || k
+  options.value = options.value || v
+  options.resolveKey = options.resolveKey || (k => k as string)
 
   const resolveOption = (index: keyof PackOptions<T>) => {
     const arr = (asArray(options?.[index]))
@@ -18,9 +22,11 @@ export function packObject<T extends Record<string, any>>(input: T, options?: Pa
   k = resolveOption('key') || k
   v = resolveOption('value') || v
 
-  const keyPrefix = input.key ? `key-${input.key}-` : '' || ''
+  const keyPrefix = input.key ? `${InternalKeySymbol}${input.key}-` : '' || ''
+
+  const key = options.resolveKey(input[k])
 
   return {
-    [`${keyPrefix}${input[k]}`]: input?.[v],
+    [`${keyPrefix}${key}`]: input?.[v],
   } as Partial<T>
 }
