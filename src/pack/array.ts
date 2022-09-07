83e6918f370cc0644a1477e0b27d96caca11192f
Harlan Wilton <harlan@harlanzw.com>
2022-09-07 18:30:38 +1000

    fix: handle de-dupe key better

--- src/pack/array.ts
+++ src/pack/array.ts
@@ -1,18 +1,19 @@
 import type { PackArrayOptions } from '../types'
-import { packObject } from './object'
+import { InternalKeySymbol, packObject } from './object'
 
 export function packArray<T extends Record<string, any>[]>(input: T, options?: PackArrayOptions<T>): Partial<Record<string, any>> {
   const packed: Partial<Record<string, any>> = {}
   for (const i of input) {
     const packedObj = packObject(i, options)
     const pKey = Object.keys(packedObj)[0] as keyof typeof packed
+    const isDedupeKey = pKey.startsWith(InternalKeySymbol)
 
-    if (!pKey.startsWith('key-') && packed[pKey]) {
+    if (!isDedupeKey && packed[pKey]) {
       packed[pKey] = Array.isArray(packed[pKey]) ? packed[pKey] : [packed[pKey]]
       packed[pKey].push(Object.values(packedObj)[0])
     }
     else {
-      packed[pKey.startsWith('key-') ? (pKey.split('-').pop() || pKey) : pKey] = packedObj[pKey]
+      packed[isDedupeKey ? (pKey.split('-').slice(1).join('-') || pKey) : pKey] = packedObj[pKey]
     }
   }
   return packed
